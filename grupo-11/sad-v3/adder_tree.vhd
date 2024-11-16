4cd74178090ee95e12cdb3706aab37dbadbb8b01
jpoanders <jpoanders@gmail.com>
2024-11-16 16:50:16 -0300

    Torna adder_tree funcional

--- grupo-11/sad-v3/adder_tree.vhd
+++ grupo-11/sad-v3/adder_tree.vhd
@@ -1,43 +1,43 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
-use ieee.math_real.all;
+USE ieee.math_real.ALL;
 
-entity adder_tree is 
-    generic (
-        width: POSITIVE; -- width := B*P
-        B: POSITIVE;
-        P: POSITIVE
-    );
+ENTITY adder_tree IS
+	GENERIC (
+		width : POSITIVE := 16; -- width := B*P
+		B : POSITIVE := 8;
+		P : POSITIVE := 2
+	);
 
-    port(
-        input_vector: in std_logic_vector(width - 1 downto 0);
-        result: out std_logic_vector((B + log2(P)) - 1 downto 0)
-    );
-end adder_tree;
+	PORT (
+		input_vector : IN STD_LOGIC_VECTOR(width - 1 DOWNTO 0);
+		result : OUT STD_LOGIC_VECTOR((B + INTEGER(log2(real(P)))) - 1 DOWNTO 0)
+	);
+END adder_tree;
 
-architecture Behavioral of adder_tree is
-    signal result_left_tree, result_right_tree: std_logic_vector(B + log2(P/2) - 1 downto 0);
+ARCHITECTURE Behavioral OF adder_tree IS
+	SIGNAL result_left_tree, result_right_tree : STD_LOGIC_VECTOR(B + INTEGER(log2(real(P)/real(2))) - 1 DOWNTO 0);
 
-begin
-    general_case: if (P > 1) generate
-        left_tree: entity work.adder_tree
-        generic map(width/2, B, P/2)
-        port map(input_vector(width - 1 downto width/2), result_left_tree);
-        
-        right_tree: entity work.adder_tree
-        generic map(width/2, B, P/2)
-        port map(input_vector(width/2 - 1 downto 0), result_right_tree);
+BEGIN
+	general_case : IF (P > 2) GENERATE
+		left_tree : ENTITY work.adder_tree
+			GENERIC MAP(width/2, B, P/2)
+			PORT MAP(input_vector(width - 1 DOWNTO width/2), result_left_tree);
 
+		right_tree : ENTITY work.adder_tree
+			GENERIC MAP(width/2, B, P/2)
+			PORT MAP(input_vector(width/2 - 1 DOWNTO 0), result_right_tree);
+		result <= STD_LOGIC_VECTOR(unsigned('0' & result_left_tree) + unsigned('0' & result_right_tree));
 
-        result <= std_logic_vector(unsigned('0' & result_left_tree) + unsigned('0' & result_right_tree));
-    
-    end generate;
-    
-    base_case: if P = 1 generate
-        result <= std_logic_vector(unsigned('0' & input_vector(width - 1 downto width/2)) + unsigned('0' & input_vector(width/2 - 1 downto 0)));
-    end generate;
-    
+	END GENERATE;
 
+	base_case : IF P = 2 GENERATE
+		result <= STD_LOGIC_VECTOR(unsigned('0' & input_vector(width - 1 DOWNTO width/2)) + unsigned('0' & input_vector(width/2 - 1 DOWNTO 0)));
+	END GENERATE;
 
-end Behavioral;
+	pass_case : IF P = 1 GENERATE
+		result <= input_vector;
+	END GENERATE;
+
+END Behavioral;
